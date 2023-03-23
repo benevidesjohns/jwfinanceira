0c24b1b3c45af0f3e7fb79a4674e123524c887c4
WilliamJSS <williamsena088@gmail.com>
2023-03-23 11:34:26 -0300

    feat: adicionadas migrations de transacao

---
+++ database/migrations/2023_03_23_141321_transacao_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transacao', function (Blueprint $table) {
+            $table->id();
+            $table->date('data');
+            $table->double('valor');
+            $table->string('detalhes');
+            // $table->integer('fk_conta')->unsigned();
+            // $table->foreign('fk_conta')->references('id')->on('conta');
+            $table->integer('fk_tipo_transacao')->unsigned();
+            $table->foreign('fk_tipo_transacao')->references('id')->on('tipo_transacao');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transacao');
+    }
+};
